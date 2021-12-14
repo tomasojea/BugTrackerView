a7a1651ed098d3f0e3665d3204ae442a63ee1d6f
tomasojea <tomasojea0@gmail.com>
2021-12-14 20:08:22 +0000

    Form data handler added

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,10 +4,35 @@ import Box from '@mui/material/Box';
 import Select from '@mui/material/Select';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
+import {useState} from "react"
 
 
 function Form(){
+    const url=""
+    const [data, setData] = useState({
+        issue_summary:"",
+        issue_description:"",
+        identified_by_person_id:"",
+        identified_date:"",
+        related_project:"",
+        assigned_to:"",
+        status:"",
+        priority:"",
+        target_resolution_date:"",
+        created_on:"",
+        created_by:""
+    })
     
+    const handleInputChange = (e) => {
+        e.preventDefault();
+
+    const { name, value } = e.target;
+    setData({
+      ...data,
+      [name]: value,
+    });
+    console.log(data)
+  };
     return(
          <Box  component="form"
                sx={{'& .MuiTextField-root': { margin: 1, width: '40ch' },}}
@@ -17,6 +42,9 @@ function Form(){
             <FormControl>
 
                 <TextField
+                  name="identified_by_person_id"
+                  value={data.identified_by_person_id}
+                  onChange={handleInputChange}
                   required
                   id="outlined-required"
                   type="number"
@@ -24,13 +52,20 @@ function Form(){
                   defaultValue="Identified by (id number)"
                 />
                 <TextField
+                  value={data.identified_date}
+                  onChange={handleInputChange}
+                  name="identified_date"
                   required
                   id="outlined-required"
-                  label="Identified by"
+                  label="Identified date"
                   type="date"
                   defaultValue="Identified date"
+                  InputLabelProps={{ shrink: true }}
                 />
+                </FormControl>
+                {/*  
                 <TextField
+                  name="related_project"
                   required
                   id="outlined-required"
                   label="Related project"
@@ -38,6 +73,7 @@ function Form(){
                   defaultValue="Related project"
                 />
                 <TextField
+                  name="assigned_to"
                   required
                   id="outlined-required"
                   label="Assigned to"
@@ -45,34 +81,43 @@ function Form(){
                   defaultValue="Assigned to"
                 />
                 <TextField
+                  name="target_resolution_date"
                   required
                   id="outlined-required"
                   label="Target Date"
                   type="date"
                   defaultValue="Target Date"
+                  InputLabelProps={{ shrink: true }}
                 />
                  <TextField
+                  name="created_on"
                   required
                   id="outlined-required"
                   label="Created on"
                   type="date"
                   defaultValue="Created on"
+                  InputLabelProps={{ shrink: true }}
                 />
                  <TextField
+                  name ="modified_on"
                   required
                   id="outlined-required"
                   label="Modified on"
                   type="date"
                   defaultValue="Modified on"
+                  InputLabelProps={{ shrink: true }}
                 />  
-                 <TextField
+                <TextField
+                  name="modified_by"
                   required
                   id="outlined-required"
                   label="Modified by"
                   type="date"
                   defaultValue="Modified by"
+                  InputLabelProps={{ shrink: true }}
                 />
                 <TextField
+                  name="resolution_summary"
                   required
                   id="outlined-multiline-static"
                   label="Resolution Summary"
@@ -85,30 +130,30 @@ function Form(){
             <FormControl style={{minWidth: 120}}>
                 <InputLabel>Status</InputLabel>
                 <Select
+                  name="status"
                   label="Status"
                   style={{margin:7,}}
-
                 >
-                  <MenuItem  value="High">High</MenuItem>
-                  <MenuItem value="Medium">Medium</MenuItem>
-                  <MenuItem value="Low">Low</MenuItem>
+                  <MenuItem  key="High" value="High">High</MenuItem>
+                  <MenuItem key="Medium" value="Medium">Medium</MenuItem>
+                  <MenuItem key="Low" value="Low">Low</MenuItem>
                 </Select>
 
                 </FormControl>
                 <FormControl style={{minWidth: 120}}>
-                    <InputLabel>Status</InputLabel>
+                    <InputLabel>Priority</InputLabel>
                 <Select
+                  name="priority"
                   label="Priority"
                   style={{marginTop:7}}
-
                 >
-                  <MenuItem  value="Open">Open</MenuItem>
-                  <MenuItem value="Closed">Closed</MenuItem>
-                  <MenuItem value="InProgress">In progress</MenuItem>
+                  <MenuItem key="Open" value="Open">Open</MenuItem>
+                  <MenuItem key="Closed" value="Closed">Closed</MenuItem>
+                  <MenuItem key="InProgress" value="InProgress">In progress</MenuItem>
                 </Select>
-
+                
             </FormControl>
-        
+             */}
         </Box>
             
     )
